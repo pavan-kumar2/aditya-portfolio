515afbe10abfd54fbded6a1c9c78700b867db036
Pavan Kumar <pavankkarkera480@gmail.com>
2026-01-10 17:00:21 +0530

    Refactor App component to clean up imports and maintain structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Slider from './components/Slider/Slider'
+import Overlay from './components/Overlay/Overlay'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
+      <Slider />
+      {/* <Overlay /> */}
     </>
   )
 }
