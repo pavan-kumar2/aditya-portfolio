37dfc7b33b469f303283632d0d394b8d656fe6c8
Pavan Kumar <pavankkarkera480@gmail.com>
2026-01-10 16:59:11 +0530

    Header component introduced

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import "./Header.css";
+
+type Props = {};
+
+const Header = (props: Props) => {
+    const [isFullscreen, setIsFullscreen] = React.useState(false);
+
+    const openOverlay = (id: string) => () => {
+        // const overlay = document.getElementById(id)
+    };
+
+    const toggleFullscreen = () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+            setIsFullscreen(true);
+        } else {
+            document.exitFullscreen();
+            setIsFullscreen(false);
+        }
+    };
+
+    return (
+        <header className="ui-element">
+            <div className="logo">Aditya Portfolio</div>
+            <nav id="mainNav">
+                <a onClick={openOverlay("about")}>About</a>
+                <a onClick={openOverlay("testimonials")}>Testimonials</a>
+                <a onClick={openOverlay("contact")}>Contact</a>
+
+                <button onClick={toggleFullscreen}
+                    className={`fullscreen-btn ${isFullscreen ? "active" : ""}`}
+                    id="fullscreenBtn"
+                    aria-label="Toggle Fullscreen"
+                >
+                    <svg viewBox="0 0 24 24">
+                        {isFullscreen ? (
+                            <path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3"></path>
+                        ) : (
+                            <path d="M16 3h3a2 2 0 0 1 2 2v3m-18 0V5a2 2 0 0 1 2-2h3m0 18H5a2 2 0 0 1-2-2v-3m18 0v3a2 2 0 0 1-2 2h-3"></path>
+                        )}
+                    </svg>
+                </button>
+            </nav>
+            <div className="menu-toggle" id="menuToggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
