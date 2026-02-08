4647638dfbafe1017d0ca69a0dd8314d4e459665
Pavan Kumar <105868944+pavan-kumar2@users.noreply.github.com>
2026-02-08 13:10:17 +0530

    floating btn for whatsapp

--- src/components/FloatingBtn/FloatingBtn.tsx
+++ src/components/FloatingBtn/FloatingBtn.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import './FloatingBtn.css';
+import { images } from '../../utils/image';
 
 type Props = {}
 
 const FloatingBtn = (props: Props) => {
     return (
-        <div>FloatingBtn</div>
+        <button className='floating-btn'>
+            <a href="https://wa.me/919876543210">
+                <img src={images.whatsappLogo} alt="Contact Us" />
+            </a>
+        </button>
     )
 }
 
