fb365241dc4a76f8b091e26e06b9a395610704a1
Pavan Kumar <105868944+pavan-kumar2@users.noreply.github.com>
2026-02-08 10:43:50 +0530

    Footer layout introduced

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Footer.css'
+import { images } from '../../utils/image'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+    return (
+        <footer>
+            <ul className='social-links'>
+                <li>
+                    <a href="">
+                        <img src={images.instagramWhiteIcon} alt="Instagram" />
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src={images.whatsappWhiteIcon} alt="WhatsApp" />
+                    </a>
+                </li>
+            </ul>
+            <p className='copyright'>
+                © 2025 Aditya Deshmukh Photography. All rights reserved.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
