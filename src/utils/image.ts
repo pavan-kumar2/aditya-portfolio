8c68ee4035eabf7ac1df99bb994c10417bf33ec3
Pavan Kumar <105868944+pavan-kumar2@users.noreply.github.com>
2026-02-08 14:06:59 +0530

    images centralized

---
+++ src/utils/image.ts
@@ -0,0 +1,19 @@
+import instagramWhiteIcon from '../assets/images/instagram-white-icon.svg';
+import whatsappWhiteIcon from '../assets/images/whatsapp-white-icon.svg';
+import angleLeftIcon from '../assets/images/angle-left-solid-full.svg';
+import angleRightIcon from '../assets/images/angle-right-solid-full.svg';
+import whatsappLogo from '../assets/images/WhatsApp.svg.png';
+import instagramLogo from '../assets/images/Instagram_icon.png';
+
+
+export const images: Record<string, string> = {
+    instagramWhiteIcon,
+    whatsappWhiteIcon,
+    angleLeftIcon,
+    angleRightIcon,
+    whatsappLogo,
+    instagramLogo
+};
+
+
+
