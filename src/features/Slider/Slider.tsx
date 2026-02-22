4e24da5d1614da196ba643d6c67037040e58f776
Pavan Kumar <105868944+pavan-kumar2@users.noreply.github.com>
2026-02-22 18:13:09 +0530

    Refactor Slider component styles; update CSS selectors for improved specificity

--- src/features/Slider/Slider.tsx
+++ src/features/Slider/Slider.tsx
@@ -1,74 +1,74 @@
-// import React from 'react'
-import { useRef } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+// // import React from 'react'
+// import { useRef } from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
-import "swiper/css";
+// import "swiper/css";
 
-import { images } from "../../utils/image";
-
-
+// import { images } from "../../utils/image";
 
 
-type Props = {
-}
-
-const Slider = (SlideComponent: any) => {
-
-    return function NewComponent(props: any) {
-
-        const prevRef = useRef<HTMLButtonElement | null>(null);
-        const nextRef = useRef<HTMLButtonElement | null>(null);
 
 
-        return (
+// type Props = {
+// }
 
-            <div className="slider-wrapper">
-                {/* Custom navigation buttons */}
-                <button ref={prevRef} className="nav-btn prev">
-                    <img src={images.angleLeftIcon} alt="Previous" />
-                </button>
+// const Slider = (SlideComponent: any) => {
 
-                <button ref={nextRef} className="nav-btn next">
-                    <img src={images.angleRightIcon} alt="Next" />
-                </button>
+//     return function NewComponent(props: any) {
 
-                <Swiper
-                    modules={[Navigation, Pagination, Autoplay]}
-                    slidesPerView={1}
-                    spaceBetween={20}
-                    loop
-                    autoplay={{ delay: 3000, disableOnInteraction: false }}
-                    pagination={{ clickable: true }}
-                    onSwiper={(swiper) => {
-                        const navigation = swiper.params.navigation;
-
-                        // ✅ Type-safe navigation binding
-                        if (navigation && typeof navigation !== "boolean") {
-                            navigation.prevEl = prevRef.current;
-                            navigation.nextEl = nextRef.current;
-
-                            swiper.navigation.init();
-                            swiper.navigation.update();
-                        }
-                    }}
-                >
-
-                    <SwiperSlide>
-                        <img src={forest} alt="Slide 1" className="slider-img" />
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <img src={forest} alt="Slide 2" className="slider-img" />
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <img src={forest} alt="Slide 3" className="slider-img" />
-                    </SwiperSlide>
-                </Swiper>
-            </div>
+//         const prevRef = useRef<HTMLButtonElement | null>(null);
+//         const nextRef = useRef<HTMLButtonElement | null>(null);
 
 
-        )
-    }
-}
+//         return (
+
+//             <div className="slider-wrapper">
+//                 {/* Custom navigation buttons */}
+//                 <button ref={prevRef} className="nav-btn prev">
+//                     <img src={images.angleLeftIcon} alt="Previous" />
+//                 </button>
+
+//                 <button ref={nextRef} className="nav-btn next">
+//                     <img src={images.angleRightIcon} alt="Next" />
+//                 </button>
+
+//                 <Swiper
+//                     modules={[Navigation, Pagination, Autoplay]}
+//                     slidesPerView={1}
+//                     spaceBetween={20}
+//                     loop
+//                     autoplay={{ delay: 3000, disableOnInteraction: false }}
+//                     pagination={{ clickable: true }}
+//                     onSwiper={(swiper) => {
+//                         const navigation = swiper.params.navigation;
+
+//                         // ✅ Type-safe navigation binding
+//                         if (navigation && typeof navigation !== "boolean") {
+//                             navigation.prevEl = prevRef.current;
+//                             navigation.nextEl = nextRef.current;
+
+//                             swiper.navigation.init();
+//                             swiper.navigation.update();
+//                         }
+//                     }}
+//                 >
+
+//                     <SwiperSlide>
+//                         <img src={forest} alt="Slide 1" className="slider-img" />
+//                     </SwiperSlide>
+
+//                     <SwiperSlide>
+//                         <img src={forest} alt="Slide 2" className="slider-img" />
+//                     </SwiperSlide>
+
+//                     <SwiperSlide>
+//                         <img src={forest} alt="Slide 3" className="slider-img" />
+//                     </SwiperSlide>
+//                 </Swiper>
+//             </div>
+
+
+//         )
+//     }
+// }
