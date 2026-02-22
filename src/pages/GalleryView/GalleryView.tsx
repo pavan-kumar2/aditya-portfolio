b959c310b8dfdb0c47e366a4f360cae0c888fc41
Pavan Kumar <105868944+pavan-kumar2@users.noreply.github.com>
2026-02-22 22:00:10 +0530

    Add GalleryView component and styles: Implement gallery layout with image cards

---
+++ src/pages/GalleryView/GalleryView.tsx
@@ -0,0 +1,213 @@
+import React from "react";
+import { images } from "../../utils/image";
+
+import "./GalleryView.css";
+
+type Props = {};
+
+const GalleryView = (props: Props) => {
+    return (
+        <div className="gallery-view-container">
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage1} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage1} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage1} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage1} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage1} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>{" "}
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage1} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoMarriage} alt="Forest Whispers" />
+            </div>
+            <div className="card">
+                <img src={images.photoImage2} alt="Forest Whispers" />
+            </div>
+        </div>
+    );
+};
+
+export default GalleryView;
